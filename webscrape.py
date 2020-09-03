bdd0c70f97e69296e27b6e760416038e930723b7
Nodebe <nodywelete3@gmail.com>
2020-09-03 20:17:44 +0100

    added techmall

--- webscrape.py
+++ webscrape.py
@@ -2,16 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 from operator import itemgetter
 
-search_input = 'iphone x'
+search_input = 'wrist watch'
 search = '+'.join(search_input.split())
-total_list = []
+
+
+def techmall(search):
+	techmall_link = 'https://www.techmall.com.ng'
+	response = requests.get('{}/?s={}&page=search&post_type=product'.format(techmall_link, search))
+	soup = BeautifulSoup(response.text, 'html.parser')
+	items = soup.find_all(class_='inner-wrap nasa-title-bottom')
+	try:
+		products_list= []
+		for item in items:
+			products = {}
+			if (' '.join(search.split('+')).lower() or ''.join(search.split('+')).lower()) in item.find('a')['title'].lower():
+				name = item.find('a')['title']
+				link = item.find('a')['href']
+				image = item.img['src']
+				price = item.find(class_='price').get_text()
+				products['name'] = name
+				products['link'] = link
+				products['image'] = image
+				products['price'] = int(price.replace('₦','').replace(',','').split(' – ')[0])
+				total_list.append(products)
+			else:
+				continue
+	except KeyError as e:
+		pass
 
 def ziksales(search):
 	ziksales_link = 'https://ziksales.com/'
 
 	response = requests.get('{}/search?item={}'.format(ziksales_link,search))
 	soup = BeautifulSoup(response.text, 'html.parser')
-	# print(response.text)
 	items = soup.find_all(class_='card card-product')
 	try:
 		products_list = []
@@ -26,15 +49,12 @@ def ziksales(search):
 				products['link'] = link
 				products['image'] = image
 				products['price'] = int(price.strip('₦').replace(',','').split('₦')[1])
-				products_list.append(products)
+				total_list.append(products)
 			else:
 				continue
 
 	except KeyError:
 		pass
-
-	return products_list
-
 
 def payporte(search):
 	payporte_link = 'https://payporte.com'
@@ -64,14 +84,12 @@ def payporte(search):
 					products['price'] = int(price.strip('₦').replace('.00','').replace(',','').split()[0])
 				else:
 					products['price'] = 0
-				products_list.append(products)
+				total_list.append(products)
 			else:
 				continue
 			
 	except KeyError:
 		pass
-	return products_list
-
 
 def jumia(search):
 	jumia_link = 'https://www.jumia.com.ng'
@@ -96,24 +114,25 @@ def jumia(search):
 				products['price'] = int(price.strip('₦ ').replace(',','').split()[0])
 				products['image'] = image
 				products['link'] = link
-				products_list.append(products)
+				total_list.append(products)
 			else:
 				continue
 	except KeyError:
 		pass
-	return products_list
 
-payportelist = payporte(search)
-jumialist = jumia(search)
-ziksaleslist = ziksales(search)
 
-for i in payportelist:
-	total_list.append(i)
-for i in jumialist:
-	total_list.append(i)
-for i in ziksaleslist:
-	total_list.append(i)
-total_list = sorted(total_list, key=itemgetter('price'), reverse=True)
+def get_data():
+	global total_list
+	total_list = []
 
-print(total_list)
-print(len(total_list))
+	payporte(search)
+	jumia(search)
+	ziksales(search)
+	techmall(search)
+
+	total_list = sorted(total_list, key=itemgetter('price'))
+
+	print(total_list)
+	print(len(total_list))
+
+get_data()
